4cd6ed479fa1fb95920e023ed8102114c8eeb18a
sirajshaikh0704 <sirajshaikh0704@gmail.com>
2022-04-10 12:24:37 +0530

    first commit

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ def index():
             searchString = request.form['content'].replace(" ","")
             flipkart_url = "https://www.flipkart.com/search?q=" + searchString
             uClient = uReq(flipkart_url)
-            flipkartPage = uClient.read()
+            flipkartPage = uClient.read()anajupy
             uClient.close()
             flipkart_html = bs(flipkartPage, "html.parser")
             bigboxes = flipkart_html.findAll("div", {"class": "_1AtVbE col-12-12"})
